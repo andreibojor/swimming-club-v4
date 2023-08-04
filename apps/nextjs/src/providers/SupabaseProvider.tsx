31ff5b1612663cc823bf490825c277ed6542e0d5
andreibojor <bojor.andrei@gmail.com>
2023-08-04 22:37:05 +0300

    added supabase Types

--- apps/nextjs/src/providers/SupabaseProvider.tsx
+++ apps/nextjs/src/providers/SupabaseProvider.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import { Database } from "@/types_db";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
-
-// import { Database } from "@/types_db";
 
 interface SupabaseProviderProps {
   children: React.ReactNode;
@@ -12,7 +11,7 @@ interface SupabaseProviderProps {
 
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
   const [supabaseClient] = useState(() => createClientComponentClient());
-  // createClientComponentClient<Database>()
+  createClientComponentClient<Database>();
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
       {children}
