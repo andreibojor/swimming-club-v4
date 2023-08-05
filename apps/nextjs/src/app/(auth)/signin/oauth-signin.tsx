d9437e1a9f87cadff11576bae67937246f95e22f
andreibojor <bojor.andrei@gmail.com>
2023-08-05 16:49:33 +0300

    auth

--- apps/nextjs/src/app/(auth)/signin/oauth-signin.tsx
+++ apps/nextjs/src/app/(auth)/signin/oauth-signin.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import * as React from "react";
+import { useRouter } from "next/navigation";
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeMinimal, ThemeSupa } from "@supabase/auth-ui-shared";
 
 // import { useSignIn } from "@clerk/nextjs";
 // import type { OAuthStrategy } from "@clerk/types";
 
 import { Button, useToast } from "@acme/ui";
-import * as Icons from "@acme/ui/src/icons";
+
+// import * as Icons from "@acme/ui/src/icons";
 
 export function OAuthSignIn() {
-  const [isLoading, setIsLoading] = React.useState<OAuthStrategy | null>(null);
+  // const [isLoading, setIsLoading] = React.useState<OAuthStrategy | null>(null);
   // const { signIn, isLoaded: signInLoaded } = useSignIn();
   const { toast } = useToast();
 
@@ -33,32 +41,54 @@ export function OAuthSignIn() {
   //   }
   // };
 
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { sesison } = useSessionContext();
+
   return (
-    <div className="flex flex-col gap-2">
-      <Button
-        variant="outline"
-        className="bg-background"
-        // onClick={() => oauthSignIn("oauth_github")}
-      >
-        {isLoading === "oauth_github" ? (
-          <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.GitHub className="mr-2 h-4 w-4" />
-        )}
-        Github
-      </Button>
-      <Button
-        variant="outline"
-        className="bg-background"
-        // onClick={() => oauthSignIn("oauth_google")}
-      >
-        {isLoading === "oauth_google" ? (
-          <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.Google className="mr-2 h-4 w-4" />
-        )}
-        Google
-      </Button>
-    </div>
+    // TODO: set icons from left to right
+
+    <Auth
+      supabaseClient={supabaseClient}
+      appearance={{
+        theme: ThemeSupa,
+        variables: {
+          default: {
+            colors: {
+              brand: "#404040",
+              brandAccent: "#22c55e",
+            },
+          },
+        },
+      }}
+      providers={["google", "facebook"]}
+    />
+
+    // <div className="flex flex-col gap-2">
+    //   <Button
+    //     variant="outline"
+    //     className="bg-background"
+    //     // onClick={() => oauthSignIn("oauth_github")}
+    //   >
+    //     {isLoading === "oauth_github" ? (
+    //       <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
+    //     ) : (
+    //       <Icons.GitHub className="mr-2 h-4 w-4" />
+    //     )}
+    //     Github
+    //   </Button>
+    //   <Button
+    //     variant="outline"
+    //     className="bg-background"
+    //     // onClick={() => oauthSignIn("oauth_google")}
+    //   >
+    //     {isLoading === "oauth_google" ? (
+    //       <Icons.Spinner className="mr-2 h-4 w-4 animate-spin" />
+    //     ) : (
+    //       <Icons.Google className="mr-2 h-4 w-4" />
+    //     )}
+    //     Google
+    //   </Button>
+    // </div>
   );
 }
--- apps/nextjs/src/app/(auth)/signin/page.tsx
+++ apps/nextjs/src/app/(auth)/signin/page.tsx
@@ -1,14 +1,14 @@
 import type { Route } from "next";
 import Link from "next/link";
 
-import { EmailSignIn } from "./email-signin";
+// import { EmailSignIn } from "./email-signin";
 import { OAuthSignIn } from "./oauth-signin";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export default function AuthenticationPage() {
   return (
-    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+    <div className="sm:w-[350px] mx-auto flex w-full flex-col justify-center space-y-6">
       <div className="flex flex-col space-y-2 text-center">
         <h1 className="text-2xl font-semibold tracking-tight">
           Create an account
@@ -18,7 +18,7 @@ export default function AuthenticationPage() {
         </p>
       </div>
       <div className="grid gap-6">
-        <EmailSignIn />
+        {/* <EmailSignIn /> */}
 
         <div className="relative">
           <div className="absolute inset-0 flex items-center">
