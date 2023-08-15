d07fd771ba886710450fb1a3456e85f1574b5814
andreibojor <bojor.andrei@gmail.com>
2023-08-15 13:45:40 +0300

    asdf

--- apps/nextjs/src/app/(marketing)/layout.tsx
+++ apps/nextjs/src/app/(marketing)/layout.tsx
@@ -1,13 +1,12 @@
 import { Suspense, type ReactNode } from "react";
-// import { cookies } from "next/headers";
-// import Link from "next/link";
+import { cookies } from "next/headers";
+import Link from "next/link";
 import { siteConfig } from "@/app/config";
 import { SiteFooter } from "@/components/footer";
 import { MobileDropdown } from "@/components/mobile-nav";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
-// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-
-// import { buttonVariants } from "@acme/ui";
+import { buttonVariants } from "@acme/ui";
 import * as Icons from "@acme/ui/src/icons";
 
 import { MainNav } from "../(dashboard)/_components/main-nav";
@@ -25,9 +24,9 @@ export default function MarketingLayout(props: { children: ReactNode }) {
         <MobileDropdown />
         <MainNav />
         <div className="ml-auto flex items-center space-x-4">
-          {/* <Suspense>
+          <Suspense>
             <DashboardLink />
-          </Suspense> */}
+          </Suspense>
         </div>
       </nav>
 
@@ -37,27 +36,27 @@ export default function MarketingLayout(props: { children: ReactNode }) {
   );
 }
 
-// async function DashboardLink() {
-//   const supabase = createServerComponentClient({
-//     cookies: cookies,
-//   });
-//   const { data, error } = await supabase.auth.getSession();
+async function DashboardLink() {
+  const supabase = createServerComponentClient({
+    cookies: cookies,
+  });
+  const { data, error } = await supabase.auth.getSession();
 
-//   if (!data) {
-//     return (
-//       <Link href="/signin" className={buttonVariants({ variant: "outline" })}>
-//         Sign In
-//         <Icons.ChevronRight className="ml-1 h-4 w-4" />
-//       </Link>
-//     );
-//   }
-//   return (
-//     <Link
-//       href={`/dashboard`}
-//       className={buttonVariants({ variant: "outline" })}
-//     >
-//       Dashboard
-//       <Icons.ChevronRight className="ml-1 h-4 w-4" />
-//     </Link>
-//   );
-// }
+  if (!data) {
+    return (
+      <Link href="/signin" className={buttonVariants({ variant: "outline" })}>
+        Sign In
+        <Icons.ChevronRight className="ml-1 h-4 w-4" />
+      </Link>
+    );
+  }
+  return (
+    <Link
+      href={`/dashboard`}
+      className={buttonVariants({ variant: "outline" })}
+    >
+      Dashboard
+      <Icons.ChevronRight className="ml-1 h-4 w-4" />
+    </Link>
+  );
+}
