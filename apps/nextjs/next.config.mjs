5f795a4254c4542c0bdd7e291129e8ca392a0bbf
andreibojor <bojor.andrei@gmail.com>
2023-08-25 19:03:49 +0300

    added auth modal, fixed next config mjs

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,22 +1,13 @@
-// Importing env files here to validate on build
-// import "./src/env.mjs";
-
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds and Linting.
- */
-// !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
-
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@acme/api", "@acme/db", "@acme/stripe", "@acme/ui"],
   pageExtensions: ["ts", "tsx", "mdx"],
   experimental: {
     mdxRs: true,
     serverActions: true,
   },
-
   images: {
     domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
   },
