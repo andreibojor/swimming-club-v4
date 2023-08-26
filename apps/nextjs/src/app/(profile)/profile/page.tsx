5e635743bc883853341072572f3a79ebc448ce9e
andreibojor <bojor.andrei@gmail.com>
2023-08-26 19:20:14 +0300

    ereorerer;ejk

--- apps/nextjs/src/app/(profile)/profile/page.tsx
+++ apps/nextjs/src/app/(profile)/profile/page.tsx
@@ -82,10 +82,10 @@ export default async function ProfilePage() {
           <Card className="w-full md:w-1/3">
             <CardHeader>
               <Avatar className="h-[80px] w-[80px]">
-                <AvatarImage src={userDetails.avatar_url} alt="rick" />
+                {/* <AvatarImage src={userDetails.avatar_url} alt="rick" /> */}
                 <AvatarFallback>A F </AvatarFallback>
               </Avatar>
-              <CardTitle>{userDetails.full_name}</CardTitle>
+              {/* <CardTitle>{userDetails.full_name}</CardTitle> */}
             </CardHeader>
             <CardContent>
               <Separator className="my-4" />
@@ -95,7 +95,7 @@ export default async function ProfilePage() {
                 </h4>
                 <div className="flex flex-col justify-between space-y-4">
                   <p className="text-sm font-medium leading-none">
-                    Email: {userDetails.email}
+                    {/* Email: {userDetails.email} */}
                   </p>
                   <p className="text-sm font-medium leading-none">
                     Status: Active !!
