04d4c730d98808b75cf790a9d6a04663c52b3121
andreibojor <bojor.andrei@gmail.com>
2023-08-26 16:27:16 +0300

    added unstyled loading pages

---
+++ apps/nextjs/src/app/loading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+// import { BounceLoader } from "react-spinners";
+
+const Loading = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      {/* <BounceLoader color="#22c55e" size={40} /> */}
+      <h1>LOADINGGGGGG</h1>
+    </div>
+  );
+};
+
+export default Loading;
