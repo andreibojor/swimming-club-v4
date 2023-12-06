ccf7a95b6db9cc2661785827c273c2d8833f7a36
andreibojor <bojor.andrei@gmail.com>
2023-12-06 11:55:32 +0200

    Fix bug in login functionality

--- apps/nextjs/src/components/multi-step-form.tsx
+++ apps/nextjs/src/components/multi-step-form.tsx
@@ -83,7 +83,6 @@ type ProfileFormValues = z.infer<typeof profileFormSchema>;
 // This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {
   medicalCertificate: null,
-  userRole: "student",
 };
 
 export function MultiStepForm({ userDetails }) {
@@ -164,11 +163,7 @@ export function MultiStepForm({ userDetails }) {
       description: (
         <>
           <h1>user details</h1>
-          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-            {/* <code className="text-white">
-              {JSON.stringify(userDetails, null, 2)}
-            </code> */}
-          </pre>
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4"></pre>
           <h1>data</h1>
           <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
             <code className="text-white">{JSON.stringify(data, null, 2)}</code>
@@ -196,210 +191,136 @@ export function MultiStepForm({ userDetails }) {
 
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              {/* STEP 1 */}
-              <motion.div
-                className={cn("space-y-3", { hidden: formStep === 1 })}
-                animate={{
-                  translateX: `-${formStep * 100}%`,
-                }}
-              >
-                <RadioGroup
-                  defaultValue={defaultValues.userRole}
-                  className="grid grid-cols-2 gap-4"
-                  {...form.register("userRole")}
-                >
-                  <div>
-                    <RadioGroupItem
-                      value="student"
-                      id="student"
-                      className="peer sr-only"
-                      {...form.register("userRole")}
-                    />
-                    <Label
-                      htmlFor="student"
-                      className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 text-lg hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+              <FormField
+                control={form.control}
+                name="userRole"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Select your role</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
                     >
-                      <Icons.User className="mb-3 h-10 w-10" />
-                      Student
-                    </Label>
-                  </div>
-                  <div>
-                    <RadioGroupItem
-                      value="parent"
-                      id="parent"
-                      className="peer sr-only"
-                      {...form.register("userRole")}
-                    />
-                    <Label
-                      htmlFor="parent"
-                      className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 text-lg hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select your user role" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="parent">Parent</SelectItem>
+                        <SelectItem value="student">Student</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormDescription>
+                Select the desired payment method.
+              </FormDescription>
+
+              <FormField
+                control={form.control}
+                name="phoneNumber"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Phone</FormLabel>
+                    <FormControl>
+                      <Input placeholder="0751123456" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="swimmerLevel"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Performance Level</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
                     >
-                      <Icons.Users className="mb-3 h-10 w-10" />
-                      Parinte
-                    </Label>
-                  </div>
-                </RadioGroup>
-                <FormDescription>
-                  Select the desired payment method.
-                </FormDescription>
-              </motion.div>
-
-              {/* STEP 2 */}
-              <motion.div
-                className={cn("space-y-3", { hidden: formStep === 0 })}
-                animate={{
-                  translateX: `${100 - formStep * 100}%`,
-                }}
-              >
-                <FormField
-                  control={form.control}
-                  name="phoneNumber"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Phone</FormLabel>
                       <FormControl>
-                        <Input placeholder="0751123456" {...field} />
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select the level of your performance" />
+                        </SelectTrigger>
                       </FormControl>
-                      {/* <FormDescription>
-                        This is your public display name. It can be your real
-                        name or a pseudonym. You can only change this once every
-                        30 days.
-                      </FormDescription> */}
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+                      <SelectContent>
+                        <SelectItem value="FALSE">Beginner</SelectItem>
+                        <SelectItem value="TRUE">Advanced</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      You can request your swimming teacher to promote you
+                      <Link href="/examples/forms">email settings</Link>.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                <FormField
-                  control={form.control}
-                  name="swimmerLevel"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Performance Level</FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select the level of your performance" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="FALSE">Beginner</SelectItem>
-                          <SelectItem value="TRUE">Advanced</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormDescription>
-                        You can request your swimming teacher to promote you
-                        <Link href="/examples/forms">email settings</Link>.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="pool"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Pool Location</FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select the level of your performance" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          <SelectItem value="Cluj-Napoca">
-                            Cluj-Napoca
-                          </SelectItem>
-                          <SelectItem value="Dej">Dej</SelectItem>
-                          <SelectItem value="Sancraiu">Sâncraiu</SelectItem>
-                        </SelectContent>
-                      </Select>
-                      <FormDescription>
-                        You can request your swimming teacher to promote you
-                        <Link href="/examples/forms">email settings</Link>.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="medicalCertificate"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Medical Certificate</FormLabel>
+              <FormField
+                control={form.control}
+                name="pool"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Pool Location</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
                       <FormControl>
-                        <Input
-                          type="file"
-                          accept=".pdf"
-                          placeholder="MedicalCertificate.pdf"
-                          // Use event.target.files to access the uploaded file
-                          onChange={(e) => {
-                            // Update the form state with the selected file
-                            field.onChange(e.target.files?.[0]);
-                          }}
-                        />
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select the level of your performance" />
+                        </SelectTrigger>
                       </FormControl>
-                      <FormDescription>
-                        Please upload your medical certificate in .pdf format.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </motion.div>
-              <div className="flex w-full justify-between">
-                <Button
-                  type="button"
-                  className={cn({ hidden: formStep == 0 })}
-                  onClick={() => setFormStep(0)}
-                >
-                  Go Back
-                </Button>
+                      <SelectContent>
+                        <SelectItem value="Cluj-Napoca">Cluj-Napoca</SelectItem>
+                        <SelectItem value="Dej">Dej</SelectItem>
+                        <SelectItem value="Sancraiu">Sâncraiu</SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormDescription>
+                      You can request your swimming teacher to promote you
+                      <Link href="/examples/forms">email settings</Link>.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                <Button
-                  type="button"
-                  className={cn("ml-auto", {
-                    hidden: formStep == 1,
-                  })}
-                  onClick={() => {
-                    form.trigger("userRole");
-                    console.log(form.trigger(["userRole"]));
+              <FormField
+                control={form.control}
+                name="medicalCertificate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Medical Certificate</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="file"
+                        accept=".pdf"
+                        placeholder="MedicalCertificate.pdf"
+                        // Use event.target.files to access the uploaded file
+                        onChange={(e) => {
+                          // Update the form state with the selected file
+                          field.onChange(e.target.files?.[0]);
+                        }}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Please upload your medical certificate in .pdf format.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                    // const phoneNumberState = form.getFieldState("phoneNumber");
-                    // const swimmerLevelState =
-                    //   form.getFieldState("swimmerLevel");
-                    // const medicalCertificateState =
-                    //   form.getFieldState("medicalCertificate");
-                    // if (!phoneNumberState.isDirty || phoneNumberState.invalid)
-                    //   return;
-                    // if (!swimmerLevelState.isDirty || swimmerLevelState.invalid)
-                    //   return;
-                    // if (
-                    //   !medicalCertificateState.isDirty ||
-                    //   medicalCertificateState.invalid
-                    // )
-                    //   return;
-
-                    setFormStep(1);
-                  }}
-                >
-                  Next Step
-                </Button>
-
-                <Button type="submit" className={cn({ hidden: formStep == 0 })}>
-                  Submit
-                </Button>
-              </div>
+              <Button type="submit">Submit</Button>
             </form>
           </Form>
         </DialogContent>
