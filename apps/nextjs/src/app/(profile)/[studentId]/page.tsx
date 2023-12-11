d74560bfd73701aad5e3d74848e3d6401a4593de
andreibojor <bojor.andrei@gmail.com>
2023-12-11 16:25:05 +0200

    Fix typo in variable name

--- apps/nextjs/src/app/(profile)/[studentId]/page.tsx
+++ apps/nextjs/src/app/(profile)/[studentId]/page.tsx
@@ -210,7 +210,7 @@ export default async function ProfilePage({
                   sortedStudentsByParent={sortedStudentsByParent}
                   dates={dates}
                   products={products}
-                  allStudentDetail={allStudentDetails}
+                  allStudentDetails={allStudentDetails}
                 />
               </CardContent>
             )}
